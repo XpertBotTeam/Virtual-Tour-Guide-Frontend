7be007d1e0be111714dafd8a4237cbbbe3c03f6e
hadimakki387 <hmakki387@gmail.com>
2023-08-15 21:22:59 +0300

    created navBar

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,8 +1,12 @@
+import NavBar from '@/components/NavBar'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div>
+        <NavBar/>
+        
+    </div>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='w-full p-4 shadow-md shadow-neutral-300 flex justify-between items-center'>
+        <div>Logo</div>
+        <div className='flex justify-center items-center gap-4 '>
+            <Link href={"/sign-in"}>Sign In</Link>
+            <Link href={"/sign-up"}>Sign Up</Link>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
