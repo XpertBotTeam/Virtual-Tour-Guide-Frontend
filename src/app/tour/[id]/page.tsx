b0c08713e2cc23d9295d7910dca0073c002eb428
hadimakki387 <hmakki387@gmail.com>
2023-09-24 18:50:26 +0300

    created a page for the siingle tour

---
+++ src/app/tour/[id]/page.tsx
@@ -0,0 +1,10 @@
+import Index from '@/components/tour view/Index'
+import React from 'react'
+
+function page() {
+  return (
+    <Index/>
+  )
+}
+
+export default page
---
+++ src/components/tour view/Index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../global/NavBar'
+import Footer from '../global/Footer'
+
+function Index() {
+  return (
+    <div>
+        <NavBar/>
+        <div className='h-[90vh]'>
+            
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Index
